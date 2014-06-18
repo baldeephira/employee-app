db2d83015c503bc64ba7405dba680bc4b6a0ab69
Baldeep Hira <baldeephira@gmail.com>
2014-06-17 23:52:04 -0700

    Using UTC Timezone with Gson * Modified JsonUtil to create a customized version of Gson using GsonBuilder * It uses UTC date formatting for all serialized dates * It uses UTC timezone for formatted values * Created custom instances JsonSerializer and JsonDeserializer to achieve this

--- api/src/main/java/net/bhira/sample/api/util/JsonUtil.java
+++ api/src/main/java/net/bhira/sample/api/util/JsonUtil.java
@@ -23,8 +23,21 @@
  */
 package net.bhira.sample.api.util;
 
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 /**
  * Class providing some JSON utilities.
@@ -33,14 +46,55 @@ import com.google.gson.GsonBuilder;
  */
 public class JsonUtil {
 
+	private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+	private static final String TIMEZONE = "UTC";
+
 	/**
-	 * Create a customized version of Gson that handles Date formats and timezones correctly.
+	 * Create a customized version of Gson that handles Date formats and timezones correctly. It
+	 * uses UTC date format for parsing and formating dates and expects the date string values to be
+	 * formatted in UTC timezone.
 	 * 
 	 * @return an instance of {@link com.google.gson.Gson}.
 	 */
 	public static Gson createGson() {
+
+		// create a custom serializer for dates
+		JsonSerializer<Date> serializer = new JsonSerializer<Date>() {
+			@Override
+			public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context) {
+				if (src == null) {
+					return null;
+				}
+				SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+				formatter.setTimeZone(TimeZone.getTimeZone(TIMEZONE));
+				return new JsonPrimitive(formatter.format(src));
+			}
+		};
+
+		// create a custom de-serializer for dates
+		JsonDeserializer<Date> deserializer = new JsonDeserializer<Date>() {
+			@Override
+			public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+					throws JsonParseException {
+				if (json == null) {
+					return null;
+				}
+				String date = json.getAsString();
+				SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+				formatter.setTimeZone(TimeZone.getTimeZone(TIMEZONE));
+				try {
+					return formatter.parse(date);
+				} catch (ParseException e) {
+					throw new JsonParseException("Error parsing date " + date, e);
+				}
+
+			}
+		};
+
+		// create a custom gson that uses the custom date serializer/deserializer
 		GsonBuilder gsonBuilder = new GsonBuilder();
-		gsonBuilder.setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		gsonBuilder.registerTypeAdapter(Date.class, serializer);
+		gsonBuilder.registerTypeAdapter(Date.class, deserializer);
 		return gsonBuilder.create();
 	}
 
